af4e5174784301c23bb1d41ae3fd200438cce864
jakob krainz <jakob@hawo-net.de>
2011-11-27 13:28:42 +0100

    foo

--- scripts/src/parse_allconfigs.py
+++ scripts/src/parse_allconfigs.py
@@ -1,9 +1,65 @@
 """ support for parsing user, group and access configuration files """
 
 import os
-import parse_users
-import parse_groups
-import parse_access
+import string
+from Rule import ReadRule, InitRule, WriteRule, DenyRule
+
+def parse_users(usersfile):
+    ret = []
+    for line in usersfile:
+        x = line.split()
+        for y in x:
+            if y[0] == '#':
+                break
+            ret.append(y)
+    return ret
+
+def parse_groups(groupsfile):
+    ret = {}
+    currgroup = None
+    for line in groupsfile:
+        isContLine = (line[0] in string.whitespace or line[0] == '#')
+        if line.find('#') != -1:
+            line = line[:line.find('#')]
+        if line.find(':') == -1:
+            if currgroup == None or not isContLine:
+                raise Exception("could not parse group file")
+            spline = line.split()
+            for uname in spline:
+                ret[currgroup].add(uname)
+        else:
+            groupcand = (line[:line.find(':')]).split()
+            groupmemb = (line[line.find(':')+1:]).split()
+            if len(groupcand) != 1 or groupcand[0] == '':
+                raise Exception("could not parse group file")
+            currgroup = groupcand[0]
+            if currgroup in ret:
+                raise Exception("group defined twice")
+            ret[currgroup] = set()
+            for uname in groupmemb:
+                ret[currgroup].add(uname)                
+    return ret
+
+def parse_access(accessconffile):
+    rulelist = []
+    for line in accessconffile:
+        line = line.strip()
+        if line == '' or line[0] == '#':
+            continue
+        spline = line.split()
+        if spline[0] == 'init':
+            rulelist.append(InitRule(spline[1:]))
+        elif spline[0] == 'read':
+            rulelist.append(ReadRule(spline[1:]))
+        elif spline[0] == 'write':
+            rulelist.append(WriteRule(spline[1:]))
+        elif spline[0] == 'deny':
+            rulelist.append(DenyRule(spline[1:]))
+        else:
+            raise Exception("could not parse access configuration file")
+        pass
+    return rulelist
+
 
 def parse_allconfigs(confdir):
 
