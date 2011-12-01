7604a71b148205d1a647b43696a132876fa8d4ad
jakob krainz <jakob@hawo-net.de>
2011-12-01 18:53:13 +0100

    foo

--- scripts/src/parse_allconfigs.py
+++ scripts/src/parse_allconfigs.py
@@ -5,72 +5,42 @@ import ConfigParser
 import string
 from Rule import ReadRule, InitRule, WriteRule, DenyRule
 
-def parse_users(usersfile):
-    ret = []
-    for line in usersfile:
-        x = line.split()
-        for y in x:
-            if y[0] == '#':
-                break
-            ret.append(y)
-    return ret
-
-def parse_groups(groupsfile):
-    ret = {}
-    currgroup = None
-    for line in groupsfile:
-        isContLine = (line[0] in string.whitespace or line[0] == '#')
-        if line.find('#') != -1:
-            line = line[:line.find('#')]
-        line = line.strip()
-        if line == '':
-            continue
-        if line.find(':') == -1:
-            if currgroup == None or not isContLine:
-                raise Exception("could not parse group file")
-            spline = line.split()
-            for uname in spline:
-                ret[currgroup].append(uname)
-        else:
-            groupcand = (line[:line.find(':')]).split()
-            groupmemb = (line[line.find(':')+1:]).split()
-            if len(groupcand) != 1 or groupcand[0] == '':
-                raise Exception("could not parse group file")
-            currgroup = groupcand[0]
-            if currgroup in ret:
-                raise Exception("group defined twice")
-            ret[currgroup] = []
-            for uname in groupmemb:
-                ret[currgroup].append(uname)                
-    return ret
-
 def parse_access(accessconffile):
-    rulelist = []
-    for line in accessconffile:
-        line = line.strip()
-        if line == '' or line[0] == '#':
-            continue
-        spline = line.split()
-        if spline[0] == 'init':
-            rulelist.append(InitRule(spline[1:]))
-        elif spline[0] == 'read':
-            rulelist.append(ReadRule(spline[1:]))
-        elif spline[0] == 'write':
-            rulelist.append(WriteRule(spline[1:]))
-        elif spline[0] == 'deny':
-            rulelist.append(DenyRule(spline[1:]))
-        else:
-            raise Exception("could not parse access configuration file")
-        pass
-    return rulelist
+    p = ConfigParser.RawConfigParser()
+    p.readfp(accessconffile)
+    accessdict = {}
+    for path in p.sections():
+        accessdict.setdefault(path, {})
+        for userorgroup, accesstype in p.items(path):
+            if userorgroup[0] == '@':
+                prefix = 'group'
+                realname = userorgroup[1:]
+            else:
+                prefix = 'user'
+                realname = userorgroup
+            accessdict[path].setdefault(prefix, {})
+            if 'r' in accesstype:
+                accessdict[path][prefix].setdefault("read", [])
+                accessdict[path][prefix]["read"].append(realname)
+            if 'w' in accesstype:
+                accessdict[path][prefix].setdefault("write", [])
+                accessdict[path][prefix]["write"].append(realname)
+            if 'C' in accesstype:
+                accessdict[path][prefix].setdefault("create", [])
+                accessdict[path][prefix]["create"].append(realname)
+    return accessdict
 
 def parse_conf(conffile):
     p = ConfigParser.RawConfigParser()
     p.readfp(conffile)
     optdict = {}
+    groupdict = {}
     for option in ['repopath', 'htpasswdpath', 'sshauthkeyspath']:
         optdict[option] = os.path.expanduser(p.get('paths', option))
-    return optdict
+    for group in p.options('groups'):
+        groupdict[group] = [x.strip() for x in p.get('groups', group).split(',')]
+    userlist = [x.strip() for x in p.get('users', 'users').split(',')]
+    return (optdict, groupdict, userlist)
 
 def valid_name(name):
     validchars = string.letters + string.digits + '_-+&'
@@ -80,25 +50,22 @@ def valid_name(name):
     return True
 
 def parse_allconfigs(confdir):
-    userFile = os.path.join(confdir, 'users')
-    groupFile = os.path.join(confdir, 'groups')
     accessFile = os.path.join(confdir, 'access')
     confFile = os.path.join(confdir, 'config')
 
-    userfd = open(userFile)
-    groupfd = open(groupFile)
     accessfd = open(accessFile)
     conffd = open(confFile)
 
-    userlist = parse_users(userfd)
-    groupdict = parse_groups(groupfd)
-    accessRuleList = parse_access(accessfd)
-    confdict = parse_conf(conffd)
+    accessdict = parse_access(accessfd)
+    confdict, groupdict, userlist = parse_conf(conffd)
 
     accessfd.close()
-    groupfd.close()
-    userfd.close()
     conffd.close()
+
+    print userlist
+    print confdict
+    print groupdict
+    print accessdict
 
     # now some sanity checks:
     for u in userlist:
@@ -116,14 +83,17 @@ def parse_allconfigs(confdir):
             if not u in userlist:
                 print "Warning: Group %s refers to undefined user %s" %(g, u)
                 groupdict[g].remove(u)
-    for r in accessRuleList:
-        for g in r.groupRestr:
-            if not g in groupdict:
-                print "Warning: Rule %s refers to undefined group %s" %(r, g)
-                r.groupRestr.remove(g)
-        for u in r.userRestr:
-            if not u in userlist:
-                print "Warning: Rule %s refers to undefined user %s" %(r, u)
-                r.userRestr.remove(u)
-    
+    for repo in accessdict:
+        
+        for ug in accessdict[repo]:
+            for accesstype in accessdict[repo][ug]:
+                for userorgroup in accessdict[repo][ug][accesstype]:
+                    print "debug_foo:" , repo, userorgroup, ug
+                    if ug == 'user' and not userorgroup in userlist:
+                        print "Warning: repo access to %s refers to undefined user %s" %(repo, userorgroup)
+                        accessdict[repo][ug][accesstype].remove(userorgroup)
+                    if ug == 'group' and not userorgroup in groupdict:
+                        print "Warning: repo access to %s refers to undefined group %s" %(repo, userorgroup)
+                        accessdict[repo][ug][accesstype].remove(userorgroup)
+    exit(0)
     return { "userlist": userlist, "groupdict": groupdict, "accessRuleList": accessRuleList, "confdict": confdict }
--- scripts/src/repo.py
+++ scripts/src/repo.py
@@ -1,3 +1,4 @@
+from mercurial import ui, hg
 import os
 
 def list_all_repos(repopath):
@@ -10,3 +11,23 @@ def list_all_repos(repopath):
         if os.access(os.path.join(p, '.hg/ADMINISTRATED_BY_HGADMIN'), os.F_OK):
             repolist.append(p)
     return repolist
+
+def allow_readaccess(repo, user):
+    repo = hg.repository(ui.ui(), repo)
+    denyread = ui.config("web", "deny_read")
+    allowread = ui.config("web", "allow_read")
+    if denyread == ['*'] or user in denyread:
+        return False
+    if allowread == ['*'] or user in allowread:
+        return True
+    return False
+
+def allow_writeaccess(repo, user):
+    repo = hg.repository(ui.ui(), repo)
+    denywrite = ui.config("web", "deny_write")
+    allowwrite = ui.config("web", "allow_write")
+    if denywrite == ['*'] or user in denywrite:
+        return False
+    if allowwrite == ['*'] or user in allowwrite:
+        return True
+    return False
