8e9e07f05c07232a3f4059c55e44829f48987e9f
Jakob Krainz <jakob@hawo-net.de>
2012-02-29 23:55:31 +0100

    more tests; path handling sanitized...

--- test/test_create.py
+++ test/test_create.py
@@ -31,7 +31,17 @@ u1 = rw
                      ('sub/sw/subsub', True), 
                      ('sub/subsub/test', False), 
                      ('sub/sw', False), 
-                     ('sub/sw/subsub2', True)]
+                     ('sub/sw/subsub2', True),
+                     (playground + '/repos/sub/sw/subsub3', True), 
+                     (playground + '/repos/../../../sub/sw/subsub3', False),
+                     (playground + '/repos/sub/.hg/subsub3', False),
+                     ('/repos/../../../sub/sw/subsub3', False),
+                     ('/repos/sub/.hg/subsub3', False),
+                     ('../../../sub/sw/subsub3', False),
+                     ('sub/.hg/subsub3', False),
+                     ('sub/link/repos/sub/subsub2', True),
+                     ('sub/link/repos/sub/new', True),
+                     ]
 
     confdir = playground + '/confick'
     execCmd([testdict['mkconfrepo'],  confdir])
@@ -40,6 +50,7 @@ u1 = rw
     execCmd([testdict['hgadmin'], '-q', '--confdir', confdir, 'updateauth'])
     for repo in predef_repos:
         genmockuprepo(playground + '/repos/' + repo)
+    os.symlink(playground, playground + '/repos/sub/link' )
     for newRepo, creationSuccessfull in reposToCreate:
         execCmd([testdict['hgadmin'], '-q', '--confdir', confdir, 
                  'maybe_create_repo', 'u1', newRepo], expectFailure = not creationSuccessfull)
